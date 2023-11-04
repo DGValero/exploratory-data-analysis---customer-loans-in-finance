ecbdba792eee0320ced5b0af3d816f6441ae3091
DGValero <daniel@garciavalero.com>
2023-11-04 10:33:35 +0000

    adds empty python file that will be used to extract data from a cloud RDS

---
+++ db_utils.py
@@ -0,0 +1,5 @@
+''' Script to extract the data from the cloud 
+The loan payments data is stored in an AWS RDS database. In this script I create the methods which will allow me to extract the data from this database. '''
+
+class RDSDatabaseConnector:
+    pass
